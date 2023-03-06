f8bba8d393a368d484fed5e5f7230e58f260d372
Mewes Kochheim <kochheim@develop4edu.de>
2023-03-06 03:34:05 +0100

    Added debug middleware to handle POST requests

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,36 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { Form } from "multiparty";
+import { inspect } from "node:util";
+
+import type { Connect, ViteDevServer } from "vite";
+import type { ServerResponse } from "node:http";
 
 const postPreview = () => ({
-  name: "post-preview-server",
-  configurePreviewServer(server) {
-    return () => {
-      server.middlewares.use((req, res, next) => {
-        console.log(req);
-      });
-    };
+  name: "post-preview-middleware",
+  configureServer(server: ViteDevServer) {
+    server.middlewares.use(
+      (req: Connect.IncomingMessage, res: ServerResponse, next: Function) => {
+        if (req.method === "POST") {
+          console.warn("URL: ", req.url);
+
+          const form = new Form();
+          form.parse(req, function (err, fields, files) {
+            const debugOutput = inspect({ fields: fields, files: files }, false, 3);;
+
+            // Console logging
+            console.log("Form data:", debugOutput);
+
+            // Debug response
+            res.writeHead(200, { "content-type": "text/plain" });
+            res.write("Form data:\n\n");
+            res.end(debugOutput);
+          });
+        } else { 
+          next();
+        }
+      }
+    );
   },
 });
 
